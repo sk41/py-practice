daabea67de5a46e8ea70af3afe723b46f46ddb9b
sk41 <snehalsk41@gmail.com>
2018-07-16 14:07:51 -0500

    removed personal data

--- sms and email/send_sms.py
+++ sms and email/send_sms.py
@@ -1,15 +1,31 @@
 from twilio.rest import Client
+import os
 
-# Your Account SID from twilio.com/console
-account_sid = "ACafd5be955dcd2ae8fbeae077fa2fe854"
-# Your Auth Token from twilio.com/console
-auth_token  = "aa5ae4b8c675f912a022834620583815"
+def send_sms(number):
+    """
+    to send sms using twilio
+    :param number:
+    :return:
+    """
+    # Your Account SID from twilio.com/console
+    account_sid = os.environ['account_sid']
+    # Your Auth Token from twilio.com/console
+    auth_token  = os.environ['auth_token']
 
-client = Client(account_sid, auth_token)
+    client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+17372031940", #this numner has to be register on www.twilio.com
-    from_="+18054207492", #generated twilio number
-    body="Hello I am Snehal!!! how are u")
+    message = client.messages.create(
+        to=number, #this numner has to be register on www.twilio.com
+        from_="+18054207492", #generated twilio number
+        body="Hello I am Snehal!!! how are u")
 
-print(message.sid)
+    print(message.sid)
+
+def main():
+    phno=input("Enter your mobile number :").strip()
+    send_sms(phno)
+
+if __name__ == '__main__':
+    main()
+
+
