4e9cf23d0f881fc934ac220c598de4bae6fe77c7
sk41 <41099873+sk41@users.noreply.github.com>
2018-07-16 14:34:35 -0500

    removed personal data (#10)
    * removed static data

--- sms and email/send_sms.py
+++ sms and email/send_sms.py
@@ -1,16 +1,31 @@
 from twilio.rest import Client
 import os
 
-# Your Account SID from twilio.com/console
-account_sid = os.environ['account_sid']
-# Your Auth Token from twilio.com/console
-auth_token  = os.environ['auth_token']
+def send_sms(number,twilio_num):
+    """
+    to send sms using twilio
+    :param number:
+    :param twilio:
+    :return none:
+    """
+    # Your Account SID from twilio.com/console
+    account_sid = os.environ['account_sid']
+    # Your Auth Token from twilio.com/console
+    auth_token  = os.environ['auth_token']
 
-client = Client(account_sid, auth_token)
+    client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+17372031940", #this numner has to be register on www.twilio.com
-    from_="+18054207492", #generated twilio number
-    body="text msg body goes here!"
+    message = client.messages.create(
+        to=number, #this numner has to be register on www.twilio.com
+        from_=twilio_num, #generated twilio number
+        body="Hello, write message body here ")
 
-print(message.sid)
+    #print(message.sid)
+
+def main():
+    phone_number=input("Enter your mobile number :").strip()
+    twilio_number=input("Enter your generated twilio number :").strip()
+    send_sms(phone_number,twilio_number)
+
+if __name__ == '__main__':
+    main()
